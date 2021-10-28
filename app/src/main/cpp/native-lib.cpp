e8f40e520ac84aa47408f9f8ab7ea53653a7db07
Ewan <houqingchen@hotmail.com>
2021-10-29 00:51:12 +0800

    * example for C++ and Java call each other

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,50 +2,112 @@
 #include <string>
 #include "log/log_tool.h"
 
-static JNIEnv *pEnv;
-static jobject jObj;
-static jmethodID setDeviceRotId;
-
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ewan_exportlib_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject  obj) {
-    pEnv = env;
-    jObj = obj;
     std::string hello = "Hello from C++";
-    LOGD("hi.");
     return env->NewStringUTF(hello.c_str());
 }
 
-extern "C" JNIEXPORT jfloatArray JNICALL
-Java_com_ewan_exportlib_MainActivity_sendDeviceRot(
-        JNIEnv* env,
-        jobject obj,
-        jfloatArray array) {
-    /**
-     * Get the length of array that passed by Java call
-     */
-    int length = env->GetArrayLength(array);
-    /**
-     * Get the point of array (Cpp array)
-     */
-    jfloat* jf = env->GetFloatArrayElements(array, 0);
-    /**
-     * Per element of array plus one
-     */
-    for (int i = 0; i < length; i++) {
-        *(jf + i) += 1.0f;
-    }
-    /**
-     * Set Cpp array to Java array
-     */
-    env->SetFloatArrayRegion(array, 0, length, jf);
-    /**
-     * If GetFloatArrayElements , must release it!
-     */
-    env->ReleaseFloatArrayElements(array, jf, 0);
-    /**
-     * Return Java array
-     */
-    return array;
+static JNIEnv* g_pEnv;
+static jobject g_jobj;
+
+/**
+ * 1.保存下JNIEnv和jobject，没有返回值，不传参
+ */
+extern "C" JNIEXPORT void JNICALL
+Java_com_ewan_exportlib_MainActivity_initialize(JNIEnv* env, jobject obj) {
+    g_pEnv = env;
+    g_jobj = obj;
+    LOGD("no pass value no return");
 }
+
+/**
+ * 2.没有返回值，传参 int数组
+ */
+extern "C" JNIEXPORT void JNICALL
+Java_com_ewan_exportlib_MainActivity_passIntArray(JNIEnv* env, jobject obj, jintArray array) {
+    jint* pInt = env->GetIntArrayElements(array, 0);
+    int length = env->GetArrayLength(array);
+    int intArray[length];
+    for (int i = 0; i < length; i++) {
+        intArray[i] = *(pInt + i);
+    }
+    LOGD("passIntArray: %i,%i,%i",intArray[0], intArray[1], intArray[2]);
+}
+
+/**
+ * 3.返回值为float数组，不传参
+ */
+extern "C" JNIEXPORT jfloatArray JNICALL
+Java_com_ewan_exportlib_MainActivity_returnFloatArray(JNIEnv* env, jobject obj) {
+    float value[3] = { 1.0f, 2.0f, 3.0f};
+    int length = std::end(value) - std::begin(value);
+    jfloatArray jfArray = env->NewFloatArray(length);
+    env->SetFloatArrayRegion(jfArray, 0, length, value);
+    return jfArray;
+}
+
+/**
+ * 4.c++ call Java
+ * 无返回值，无参数
+ */
+static void Initialize() {
+    jclass clazz = g_pEnv->GetObjectClass(g_jobj);
+    jmethodID id = g_pEnv->GetMethodID(clazz, "Initialize", "()V");
+    g_pEnv->CallVoidMethod(g_jobj, id);
+}
+
+/**
+ * 5.c++ call Java
+ * 无返回值，参数为std::string
+ * @param funcName
+ */
+static void PassStringValue(std::string funcName) {
+    jclass clazz = g_pEnv->GetObjectClass(g_jobj);
+    jmethodID id = g_pEnv->GetMethodID(clazz, "PassStringValue", "(Ljava/lang/String;)V");
+    jstring str = g_pEnv->NewStringUTF(funcName.c_str());
+    g_pEnv->CallVoidMethod(g_jobj, id, str);
+}
+
+/**
+ * 6.c++ call Java
+ * 返回值为string，无参数
+ * @return
+ */
+static std::string ReturnString() {
+    jclass clazz = g_pEnv->GetObjectClass(g_jobj);
+    jmethodID id = g_pEnv->GetMethodID(clazz, "ReturnString", "()Ljava/lang/String;");
+    jstring str = (jstring)g_pEnv->CallObjectMethod(g_jobj, id);
+    const char* c_str = nullptr;
+    c_str = g_pEnv->GetStringUTFChars(str, 0);
+    g_pEnv->ReleaseStringUTFChars(str, c_str);
+    return std::string (c_str);
+}
+
+/**
+ * 被Java调起，用来调cpp的函数
+ */
+extern "C" JNIEXPORT void JNICALL
+Java_com_ewan_exportlib_MainActivity_callNative(JNIEnv* env, jobject obj) {
+    /**
+     * 4. c++ call Java
+     */
+    Initialize();
+
+    /**
+     * 5. c++ call Java
+     */
+    std::string funcName = "PassStringValue";
+    PassStringValue(funcName);
+
+    /**
+     * 6. c++ call Java
+     */
+    std::string str = ReturnString();
+    LOGD("%s", str.c_str());
+}
+
+
+
--- app/src/main/java/com/ewan/exportlib/MainActivity.java
+++ app/src/main/java/com/ewan/exportlib/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     static final String TAG = "Ewan";
 
+    static final int[] intArray = { 1, 2, 3 };
+
+//    static final float[] rotation = {1.0f, 2.0f, 3.0f, 4.0f};
+
     // Used to load the 'exportlib' library on application startup.
     static {
         System.loadLibrary("exportlib");
@@ -30,9 +34,30 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
-        // Call local method
-        float[] rot = new float[4];
-        setDeviceRot(rot);
+
+        /**
+         * 1. Java call native c++
+         * 没有返回值，无参数
+         */
+        initialize();
+
+        /**
+         * 2. Java call native c++
+         * 没有返回值，参数为int数组
+         */
+        passIntArray(intArray);
+
+        /**
+         * 3. Java call native c++
+         * 返回值为float数组，无参数
+         */
+        float[] returnFloatArray = returnFloatArray();
+        Log.d(TAG,"return float array :" + returnFloatArray[0] + "," + returnFloatArray[1] + "," + returnFloatArray[2]);
+
+        /**
+         * 调native c++ 让cpp里的函数可以被调用
+         */
+        callNative();
     }
 
     /**
@@ -42,21 +67,47 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
 
     /**
-     * A native method that pass float[] and return float[]
-     * @param rot
+     * 1.声明native method : 没有返回值，无参数
+     * */
+    public native void initialize();
+
+    /**
+     * 2.声明native method : 没有返回值，参数为 int数组
+     * @param intArray
+     */
+    public native void passIntArray(int[] intArray);
+
+    /**
+     * 3.声明native method : 返回值为float数组，无参数
      * @return
      */
-    public native float[] sendDeviceRot(float[] rot);
+    public native float[] returnFloatArray();
 
-    public void setDeviceRot(float[] rot) {
-        rot[0] = 0.0f;
-        rot[1] = 1.0f;
-        rot[2] = 2.0f;
-        rot[3] = 3.0f;
-        float[] back = sendDeviceRot(rot);
-        Log.e(TAG, String.valueOf(back[0]));
-        Log.e(TAG, String.valueOf(back[1]));
-        Log.e(TAG, String.valueOf(back[2]));
-        Log.e(TAG, String.valueOf(back[3]));
+    /**
+     * 调一个native c++来执行c++里的函数
+     */
+    public native void callNative();
+
+    /**
+     * 4.called by c++ : 无返回值，无参数
+     */
+    public void Initialize() {
+        Log.d(TAG, "native c++ call Java successful");
+    }
+
+    /**
+     * 5. called by c++ : 无返回值，参数为string
+     * @param funcName
+     */
+    public void PassStringValue(String funcName) {
+        Log.d(TAG, funcName);
+    }
+
+    /**
+     * 6. called bt c++ : 返回值为string， 无参数
+     * @return
+     */
+    public String ReturnString() {
+        return "ReturnString";
     }
 }
